eaa04438a32f1bb8ea4ce9ad218a657d400aa643
jeremy <ajcantor1@gmail.com>
2023-11-14 15:34:46 -0500

    escaped single quote in about.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 const About = () => {
     return (
         <section className="lg:w-1/3 lg:py-20 lg:flex lg:flex-col justify-center items-center">
-            <p className='text-[#ADB7BE] text-lg lg:text-xl'>
-                Hi there, I'm Jeremy. Electrical Engineer and Software Developer. I create things that live on the internet, from websites, applications, or anything in between. My interest are broad and cover a variety of bleeding-edge topics in computing.
+            <p className="text-[#ADB7BE] text-lg lg:text-xl">
+                Hi there, I&#39;m Jeremy. Electrical Engineer and Software Developer. I create things that live on the internet, from websites, applications, or anything in between. My interest are broad and cover a variety of bleeding-edge topics in computing.
             </p>
 
         </section>
