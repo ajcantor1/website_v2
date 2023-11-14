b540d28d27521ed545e25877272c9e372d9773ff
jeremy <ajcantor1@gmail.com>
2023-11-14 15:06:14 -0500

    first commit

---
+++ src/app/components/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const About = () => {
+    return (
+        <section className="lg:w-1/3 lg:py-20 lg:flex lg:flex-col justify-center items-center">
+            <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+                Hi there, I'm Jeremy. Electrical Engineer and Software Developer. I create things that live on the internet, from websites, applications, or anything in between. My interest are broad and cover a variety of bleeding-edge topics in computing.
+            </p>
+
+        </section>
+    )
+}
+
+export default About
