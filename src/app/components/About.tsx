f25e0f8fc5af298d90cb1935b7c65b7e80fda3e7
jeremy <ajcantor1@gmail.com>
2023-11-16 09:42:14 -0500

    lg:pl-4

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Projects from './Projects'
 const About = () => {
     return (
-        <main className="lg:w-1/2 pl-4 flex flex-col justify-center lg:pt-36 items-center lg:items-start">
+        <main className="lg:w-1/2 lg:pl-4 flex flex-col justify-center lg:pt-36 items-center lg:items-start">
             <div className='my-4'>
                 <p className="text-[#ADB7BE] text-lg lg:text-xl max-w-xs lg:max-w-none lg:mx-4 self-center">
                     My main focus these days is keeping up with the latest artificial intelligence trends and exploring novel ways to apply them. When I am not at computer or solder station you can find me out the gym, practicing Cuong Nhu, hanging out with my cats, or fighting crime.
